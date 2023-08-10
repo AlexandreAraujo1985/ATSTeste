db7fe14ececb9bbaf49bce81ed00f2ebb2c9f493
Alexandre Araújo <alexandrearaujo1985@gmail.com>
2023-08-10 16:37:28 -0300

    Update Curriculo.cs

--- ATSBackend/ATSBackend.Domain/Entities/Curriculo.cs
+++ ATSBackend/ATSBackend.Domain/Entities/Curriculo.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ATSBackend.Domain.Entities
 {
+    //Modificador de acesso
     public class Curriculo
     {
         public int IdCurriculo { get; set; }
